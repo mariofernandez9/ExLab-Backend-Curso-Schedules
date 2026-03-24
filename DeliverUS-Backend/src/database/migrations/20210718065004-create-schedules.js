06931ab2b75b78d30ae3c79c5f9699b8019c761c
mariofernandez9 <mariofdezus@gmail.com>
2026-03-24 19:01:17 +0100

    Ejercicio1

--- DeliverUS-Backend/src/database/migrations/20210718065004-create-schedules.js
+++ DeliverUS-Backend/src/database/migrations/20210718065004-create-schedules.js
@@ -1,7 +1,46 @@
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.createTable('Schedules', {
+      id:{
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
 
+      startTime: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+
+      endTime: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+
+      restaurantId: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        onDelete: 'CASCADE', // Se borran las reseñas de los restaurantes eliminados
+        references: {
+          model: {
+            tableName: 'Restaurants'
+            },
+          key: 'id'
+        }
+      },
+
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: new Date()
+      },
+
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: new Date()
+      }
     })
   },
 
